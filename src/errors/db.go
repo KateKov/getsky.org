e9e3c36ac8ada8a64ca00b6d319e7f6762b9b800
KateKov <kateryna_kovalenko@hotmail.com>
2018-06-25 17:49:34 +0300

    Update existing error message for user registration

--- src/errors/db.go
+++ src/errors/db.go
@@ -9,13 +9,22 @@ import (
 const DbDuplicateEntry = "Error 1062: "
 const duplicateEntryPropertyKey = "for key "
 
+// ErrorType indicates which type has db error
+type ErrorType int
+
+// Available types for errors
+const (
+	GenericDatabaseError ErrorType = iota
+	RegistrationDatabaseError
+)
+
 // IsDbValidationError checks if the error is db validation error
 func IsDbValidationError(err error) bool {
 	return strings.HasPrefix(err.Error(), DbDuplicateEntry)
 }
 
 // DatabaseErrorResponse parses database error string to user friendly error
-func DatabaseErrorResponse(err error) ValidationError {
+func DatabaseErrorResponse(err error, errorType ErrorType) ValidationError {
 	errorResponse := make([]ValidationErrorResponse, 0)
 
 	if !IsDbValidationError(err) {
@@ -28,11 +37,21 @@ func DatabaseErrorResponse(err error) ValidationError {
 
 		errorResponse = append(errorResponse, ValidationErrorResponse{
 			Key:     makeFirstLowerCase(key),
-			Message: "It already exists in a database.",
+			Message: getErrorMessage(errorType, key),
 		})
 	}
 
 	return ValidationError{Errors: errorResponse}
+}
+
+// Gets error message by specified error type
+func getErrorMessage(errorType ErrorType, key string) string {
+	switch errorType {
+	case RegistrationDatabaseError:
+		return "An account with this " + strings.ToLower(key) + " has already been registered."
+	default:
+		return "Entity with this " + strings.ToLower(key) + " already exists in database."
+	}
 }
 
 func makeFirstLowerCase(s string) string {

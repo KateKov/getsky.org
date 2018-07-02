162657259ab2aa55c801e0174a241d599a65ca5c
KateKov <kateryna_kovalenko@hotmail.com>
2018-07-02 11:46:28 +0300

    Delete unused types for errors

--- src/errors/db.go
+++ src/errors/db.go
@@ -7,15 +7,6 @@ import (
 // DbDuplicateEntry corresponds to an db error that specifies duplicating of data
 const DbDuplicateEntry = "Error 1062: "
 
-// ErrorType indicates which type has db error
-type ErrorType int
-
-// Available types for errors
-const (
-	GenericDatabaseError ErrorType = iota
-	RegistrationDatabaseError
-)
-
 // IsDbValidationError checks if the error is db validation error
 func IsDbValidationError(err error) bool {
 	return strings.HasPrefix(err.Error(), DbDuplicateEntry)

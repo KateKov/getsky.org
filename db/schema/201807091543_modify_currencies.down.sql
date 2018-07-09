be8b07a79ca2e332fe28d5fc2dedadb17b02f464
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-07-09 18:43:51 +0300

    #38 Remove country column in currency

---
+++ db/schema/201807091543_modify_currencies.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE Currencies
+	ADD COLUMN CountryCode VARCHAR(20) COLLATE utf8_unicode_ci;
+
+ALTER TABLE Currencies
+	ADD CONSTRAINT `Currencies_countries_fk` FOREIGN KEY (`CountryCode`) REFERENCES `Countries` (`Code`);
---
+++ db/schema/201807091543_modify_currencies.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE Currencies
+	DROP FOREIGN KEY `Currencies_countries_fk`;
+
+ALTER TABLE Currencies
+	DROP COLUMN CountryCode;

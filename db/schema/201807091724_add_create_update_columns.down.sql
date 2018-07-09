f3f1e3f11ae2590d669b3e889712060d07366f11
Constantine Vyrodov <constantine.vyrodov@sigma.software>
2018-07-09 18:44:10 +0300

    #38 Add CreatedAt and UpdatedAt columns

---
+++ db/schema/201807091724_add_create_update_columns.down.sql
@@ -0,0 +1,21 @@
+ALTER TABLE Adverts
+	DROP COLUMN UpdatedAt;
+
+ALTER TABLE Countries
+	DROP COLUMN CreatedAt,
+	DROP COLUMN UpdatedAt;
+
+ALTER TABLE Currencies
+	DROP COLUMN CreatedAt,
+	DROP COLUMN UpdatedAt;
+
+ALTER TABLE Messages
+	DROP COLUMN UpdatedAt;
+
+ALTER TABLE States
+	DROP COLUMN CreatedAt,
+	DROP COLUMN UpdatedAt;
+
+ALTER TABLE Users
+	DROP COLUMN CreatedAt,
+	DROP COLUMN UpdatedAt;

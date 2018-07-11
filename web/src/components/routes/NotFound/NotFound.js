5227066f0817196748becd14329aedcb77d1c7c3
KateKov <kateryna_kovalenko@hotmail.com>
2018-07-11 15:56:37 +0300

    Remove the skywire image from the NotFound ppage. Display a large 404 with text in the center

--- web/src/components/routes/NotFound/NotFound.js
+++ web/src/components/routes/NotFound/NotFound.js
@@ -4,12 +4,11 @@ import styled from 'styled-components';
 import { Flex } from 'grid-styled';
 import media from '../../../media';
 import { rem } from 'polished';
-import notFound from './images/404.svg';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 75vh;
 `;
 
 const Page = styled.div`
@@ -21,91 +20,65 @@ const Page = styled.div`
 const NotFoundContent = styled.div`
   margin: 0 auto;
   width: 100%;
-  max-width: 70rem;
+  max-width: 50rem;
   padding: 0 2rem;
   box-sizing: border-box;
 `
 
-const MessageAndImageContainer = styled(Flex)`
+const MessageContainer = styled(Flex)`
+  justify-content: center;
+  flex-direction: column;
   position: relative;
-
   ${media.sm`
     display:  flex-end;
-  `}
-  justify-content: center;
-`;
-
-const ImageWrapper = styled.div`
-  display: none;
-  padding-left: 150px;
-  max-width: 100%;
-  
-  ${media.sm`
-    display: block;
-  `}
-`;
-
-const Graphic = styled.img`
-  display: block;
-  max-width: 100%;
-`;
-
-const MessageContainer = styled(Flex)`
-  align-items: flex-start;
-  justify-content: flex-start;
-  flex-direction: column;
-  width: ${rem(280)};
-
-  ${media.sm`
-    position: absolute;
-    top: -25px;
-    left: 0;
   `}
 `;
 
 const NotFoundCode = styled.h2`
-  margin: 0;
+  margin: 0 auto;
   padding: 0;
   color:  ${props => props.theme.colors.blue};
   font-family:  ${props => props.theme.fontFamilies.sansBold};
   font-weight: 400;
   line-height: ${props => props.theme.lineHeights[0]};
   margin-bottom: 0;
-  font-size: 100px;
+  font-size: 180px;
 
   ${media.md`
-    font-size: 140px;
+    font-size: 200px;
   `}
 `;
 
 const NotFoundHeading = styled.h1`
-  margin: 0;
+  margin: 0 auto;
   padding: 0;
   color: ${props => props.theme.colors.blackAlpha7};
   font-family: ${props => props.theme.fontFamilies.sansBold};
   font-weight: 400;
   line-height: ${props => props.theme.lineHeights[0]};
   margin-bottom: 20px;
-  font-size: 32px;
+  font-size: 46px;
 
   ${media.md`
-    font-size: 36px;
+    font-size: 48px;
   `}
 `;
 
 const NotFoundMessage = styled.p`
-  font-size: 15px;
+  font-size: 20px;
   color: ${props => props.theme.colors.blackAlpha7};
   margin-bottom: 20px;
   font-family:  ${props => props.theme.fontFamilies.sans};
   line-height:${props => props.theme.lineHeights[3]}rem;
   text-transform: none;
-  text-align: left;
+  text-align: center;
 `;
 
 const NotFoundLink = styled.a`
   color: ${props => props.theme.colors.blue};
   text-decoration: none;
+  font-size: 20px;
+  text-align: center;
   font-family: ${props => props.theme.fontFamilies.sans};
 `
 
@@ -114,7 +87,6 @@ const NotFound = () => (
      <Helmet><title>Page not found</title></Helmet>
     <Page>
       <NotFoundContent>
-        <MessageAndImageContainer>
           <MessageContainer>
             <NotFoundCode>404</NotFoundCode>
             <NotFoundHeading>Page not found</NotFoundHeading>
@@ -124,10 +96,6 @@ const NotFound = () => (
             </NotFoundMessage>
             <NotFoundLink href="/">Home page</NotFoundLink>
           </MessageContainer>
-          <ImageWrapper>
-            <Graphic src={notFound} alt="" />
-          </ImageWrapper>
-        </MessageAndImageContainer>
       </NotFoundContent>
     </Page>
     </Wrapper>
